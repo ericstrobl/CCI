eaa88e51b6ca33af968fbecb8cf7898867ae68ca
Eric V. Strobl <ericvonstrobl@gmail.com>
2018-08-13 10:28:48 -0400

    Update cci.R

--- R/cci.R
+++ R/cci.R
@@ -6,8 +6,7 @@
 #' @param suffStat list of sufficient statistics needed by the CI test. E.g., the data or covariance matrix
 #' @param indepTest the CI test function
 #' @param alpha the alpha value for the CI test
-#' @param labels (use labels or p) labels of variables
-#' @param p (use p or labels) number of variables
+#' @param p the number of variables
 #' @param skeleton_pre Number of features for conditioning set. Default is 25.
 #' @param rules A logical vector indicating which of the 7 orientation rules to fire
 #' @return A list containing the partially oriented MAAG \code{paag} and statistic \code{Sta}
@@ -22,6 +21,7 @@ cci <- function (suffStat, indepTest, alpha, labels, p, skeleton_pre=NULL,
   if (verbose)
     cat("Compute Skeleton\n================\n")
 
+  # Step 1
   pdsepRes = IP_discovery(suffStat, indepTest = indepTest, alpha=alpha,p=p);
   G <- pdsepRes$G
   sepset <- pdsepRes$sepset
@@ -37,30 +37,27 @@ cci <- function (suffStat, indepTest, alpha, labels, p, skeleton_pre=NULL,
   if (verbose)
     cat("\nDirect egdes:\n=============\n")
 
+  # Step 2
   G <- v_struc(pag=G, sepset, unfVect = tripleList, verbose)
 
-  # no_pre_res <- udag2pag3(pag = G, sepset, rules = rules, unfVect = tripleList,
-  # verbose = verbose)
-
+  # Step 3
   list_pre_v <- after_v_struc(pag=G, sepset, suffStat, indepTest, alpha, verbose=verbose);
 
-  # list_pre_v = list(G=G,rules_used=rules_used);
-
+  # Step 4
   sup_sepset <- extra_dsep(list_pre_v$G, suffStat, indepTest, sepset, alpha,
                            verbose = verbose)
 
+  # Step 5
   list_e <- step_e(list_pre_v$G, sepset, sup_sepset, suffStat, indepTest, alpha, verbose = verbose,
                    rules_used = list_pre_v$rules_used)
 
-
+  # Step 6
   list_f <- step_f(list_e$pag, sepset, sup_sepset, suffStat, indepTest,
                    alpha, verbose=verbose, list_e$rules_used)
 
-
+  # Step 7
   res <- udag2pag4(pag = list_f$pag, sepset, rules = rules, unfVect = tripleList,
                    verbose = verbose, rules_used = list_f$rules_used)
-
-  colnames(res$pag) <- rownames(res$pag) <- labels
 
   return(list(maag = res$pag, pre_OR_res = list_f$pag, rules_used = res$rules_used))
 
